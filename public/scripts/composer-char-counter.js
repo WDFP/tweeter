c3938649961fd1c2490355ca498bb002ac218827
Victor Ha <victorha88@gmail.com>
2023-02-28 23:53:38 -0500

    Implement Character Counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").keyup('click', function() {
+    const maxLength = 140;
+    const textLength = $(this).val().length;
+    const charRemain = maxLength - textLength;
+    const $newCounter = $(this).parent().find('.counter');
+    $newCounter.text(charRemain);
+
+if (charRemain < 0) {
+ $newCounter.addClass("counter-color")
+} else {
+ $newCounter.removeClass("counter-color")
+}
+
+ })
+});
