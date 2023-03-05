bf893511230ab9010e40ccfbf154f7f78dc83721
Victor Ha <victorha88@gmail.com>
2023-03-05 17:34:33 -0500

    Fetching Tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,37 +1,11 @@
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-
+  $(".tweet-box").empty()
   for (let index of tweets) {
     const render = createTweetElement(index);
-    $(".tweet-box").append(render); // takes return value and appends it to the tweets container
+    $(".tweet-box").prepend(render); // takes return value and appends it to the tweets container
   }
 };
 
@@ -39,13 +13,15 @@ const createTweetElement = function (data) {
   let $tweet = $(`<article class="tweets">
   <div class="tweet-header">
        
-   <span class="tweet-username"><img src="${data.user.avatars}">  <div>${data.user.name}</div></span>
+   <span class="tweet-username"><img src="${data.user.avatars}">  <div>${
+    data.user.name
+  }</div></span>
    <span class="user- name">${data.user.handle}</span>
 </div>
 <p class="tweet-text">${data.content.text}</p>
 
 <footer>
-  <time>${data.created_at}</time>
+  <time>${timeago.format(data.created_at)}</time>
     <article class="hover-icons">
     <i class="fa-thin fa-flag-checkered"></i>
     <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -53,21 +29,27 @@ const createTweetElement = function (data) {
 </footer>
 </article>`);
 
-return $tweet;
+  return $tweet;
+};
+
+const loadTweets = function () {
+  $.ajax("/tweets", { method: "GET" }).then((tweets) => {
+    renderTweets(tweets);
+  });
 };
 
 $(document).ready(function () {
-  renderTweets(data);
   $("#request").submit(function (event) {
     event.preventDefault();
     console.log(event);
 
     $.ajax({
-      url: "/tweets/", 
+      url: "/tweets/",
       method: "post",
-      data: $(this).serialize()
-    }).then(function(res) {
-      console.log(res)
+      data: $(this).serialize(),
+    }).then(function (res) {
+      loadTweets();
     });
-  })
+  });
+  loadTweets();
 });
